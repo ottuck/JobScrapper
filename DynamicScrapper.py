e06165e2115d91c92622b491bd405c7ff4edb8d6
ottuck <vison901@gmail.com>
2024-05-20 14:31:09 +0900

    에러 수정

--- DynamicScrapper.py
+++ DynamicScrapper.py
@@ -1,48 +1,43 @@
+import os
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import time
 import csv
 
+# User-Agent 설정
+user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/90.0.4430.85 Safari/537.36")
+
 # playwright 라이브러리를 사용하여 동적 스크래핑
-p = sync_playwright().start()
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context(user_agent=user_agent)
+    page = context.new_page()
 
-browser = p.chromium.launch(headless=False)
-
-page = browser.new_page()
-
-page.goto("https://www.wanted.co.kr/")
-
-page.click("button.Aside_searchButton__Xhqq3")
-
-time.sleep(1)
-
-page.get_by_placeholder("검색어를 입력해 주세요.").fill("python")
-
-time.sleep(1)
-
-page.keyboard.press("Enter")
-
-time.sleep(1)
-
-page.click("a#search_tab_position")
-
-for _ in range(5):
+    page.goto("https://www.wanted.co.kr/")
+    page.click("button.Aside_searchButton__Xhqq3")
     time.sleep(1)
-    page.keyboard.press("End")
+    page.get_by_placeholder("검색어를 입력해 주세요.").fill("python")
+    time.sleep(1)
+    page.keyboard.press("Enter")
+    time.sleep(1)
+    page.click("a#search_tab_position")
 
-time.sleep(1)
+    for _ in range(5):
+        time.sleep(1)
+        page.keyboard.press("End")
 
-content = page.content()
-
-browser.close()
+    time.sleep(1)
+    content = page.content()
+    
+    # 브라우저 인스턴스 종료
+    browser.close()
 
 # BeautifulSoup 라이브러리를 사용하여 잡 리스트 추출
 soup = BeautifulSoup(content, "html.parser")
-
 jobs = soup.find_all("div", class_ = "JobCard_container__FqChn")
 
 job_list = []
-
 for job in jobs:
     link = f"https://www.wanted.co.kr{job.find('a')['href']}"
     title = job.find("strong", class_ = "JobCard_title__ddkwM").text
@@ -51,13 +46,21 @@ for job in jobs:
     job = {"Title": title, "Company": company, "Reward": reward, "Link": link}
     job_list.append(job)
 
-print(job_list)
-print(len(job_list))
+print(f"{len(job_list)}개의 채용 정보를 찾았습니다.")
 
 # CSV 파일로 저장
-file = open("jobs.csv", "w")
-writer = csv.writer(file)
-writer.writerow(["Title", "Company", "Reward", "Link"])
-for job in job_list:
-    writer.writerow(job.values())
-file.close()
+base_filename = "jobs"
+filename = f"{base_filename}.csv"
+file_counter = 1
+
+while os.path.exists(filename):
+    filename = f"{base_filename}_{file_counter}.csv"
+    file_counter += 1
+
+with open(filename, "w", newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Company", "Reward", "Link"])
+    for job in job_list:
+        writer.writerow(job.values())
+
+print(f"{filename} 파일로 저장되었습니다.")
